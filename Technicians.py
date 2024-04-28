c97c90eba4d99cf10cce0e11119ce78ae18cc552
aserbina28 <anna.r.serbina@gmail.com>
2024-04-28 21:45:55 +0200

    minor fix

---
+++ Technicians.py
@@ -0,0 +1,194 @@
+# Technicians.py is the IP for solving technician problem
+
+import numpy as np
+from gurobipy import *
+from Tours import feasible_tours
+
+
+# function to calculate distance
+def distance(instance, location1, location2):
+    x1 = instance.locations[location1][1]
+    y1 = instance.locations[location1][2]
+    x2 = instance.locations[location2][1]
+    y2 = instance.locations[location2][2]
+    return np.sqrt((x1-x2)**2 + (y1-y2)**2)
+
+# function to solve IP for technicians
+def IP_Technicians(instance):
+
+    tours, machines_on_tour = feasible_tours(instance)
+    '''
+    tours = [["tours"]]
+    machines_on_tour = [["machines requested"]]
+    for t in range(1, instance.numRequests+1):
+        tours.append([t])
+        # machine type requested, add to list of machine types
+        machine_type_1 = instance.requests[t][4]
+        machines_on_tour.append([machine_type_1])
+        for t2 in range(t, instance.numRequests+1):
+            if t != t2:
+                tours.append([t, t2])
+                machine_type_2 = instance.requests[t2][4]
+                machines_on_tour.append([machine_type_1, machine_type_2])
+    '''
+
+    # schedule 
+    schedule = [["schedule"]]
+    # give 1 day off for 4 days worked
+    for i in range(0,5):
+        schedule.append([])
+        for j in range(1, instance.days+1):
+            if j % 5 - i != 0:
+                schedule[i+1].append(j)
+    start = len(schedule)
+    # give 2 days off for 5 days worked
+    for i in range(0,7):
+        schedule.append([])
+        for j in range(1, instance.days+1):
+            if j % 7 != i and j % 7 != i+1:
+                schedule[start+i].append(j)
+
+    #length of tour t performed by technician p
+    def tech_tour_distance(t, p):
+        dist = 0
+        technicianLocationID = instance.technicians[p][1]
+        tourLocationIDs = []
+        for m in tours[t]:
+            tourLocationIDs.append(instance.requests[m][1])
+        # add the distance from technician start to first request 1
+        dist += distance(instance, technicianLocationID, tourLocationIDs[0]) 
+        for i in range(1, len(tourLocationIDs)-1):
+            dist += distance(instance, tourLocationIDs[i-1], tourLocationIDs[i])
+                # add distance of techncian going back to their starting place
+        dist += distance(instance, tourLocationIDs[len(tourLocationIDs)-1], technicianLocationID)    
+        return dist
+
+    # variable indicates if tour t includes machine m
+    b = {}
+    for t in range(1, len(tours)):
+        for m in range(1, instance.numRequests+1):
+            if m in tours[t]:
+                b[t,m] = 1
+            else:
+                b[t,m] = 0
+    
+    # variable indicates cost of person p completeing tour t
+    h = {}            
+    for p in range(1, instance.numTechnicians+1):
+        for t in range(1, len(tours)): 
+            h[p,t] = 0
+            # find the locations of the technician's start and each request m on the tour
+            technicianLocationID = instance.technicians[p][1]
+            tourLocationIDs = []
+            for m in tours[t]:
+                tourLocationIDs.append(instance.requests[m][1])
+            # add the $/unit of distance from technician start to first request 1
+            h[p,t] += instance.technicianDistanceCost * distance(instance, technicianLocationID, tourLocationIDs[0])
+            # add cost of distance traveled for subsequent requests 
+            for i in range(1, len(tourLocationIDs)-1):
+                h[p,t] += instance.technicianDistanceCost * distance(instance, tourLocationIDs[i-1], tourLocationIDs[i])
+            # add cost of techncian going back to their starting place
+            h[p,t] += instance.technicianDistanceCost * distance(instance, tourLocationIDs[len(tourLocationIDs)-1], technicianLocationID)
+            h[p,t] += instance.technicianDayCost
+    
+    # variable indicates if schedule s contains day d
+    e = {}
+    for s in range(1, len(schedule)):
+        for d in range(1, instance.days+1):
+            if d in schedule[s]:
+                e[s,d] = 1
+            else:
+                e[s,d] = 0
+    
+    # variable indicates whether machine request m can be delivered on day d
+    l = {}
+    for m in range(1, instance.numRequests+1):
+        for d in range(1, instance.days+1):
+            # is the day greater than (after) the first day the request is released?
+            if d > instance.requests[m][2]: 
+                l[m,d] = 1
+            else:
+                l[m,d] = 0
+    
+    # gurobi model
+    model = Model()
+
+    # decision var person p performs tour t on day d
+    y = {}
+    for p in range(1, instance.numTechnicians + 1):
+        for t in range(1, len(tours)):
+            for d in range(1, instance.days + 1):
+                # Check if the tour distance is within the technician's limit
+                if tech_tour_distance(t, p) <= instance.technicians[p][2]:
+                    # Check if the technician can install all machines in the tour
+                    if all(instance.technicians[p][m+3] for m in machines_on_tour[t]): #need to fix this -- not sure how tours work
+                        y[p, t, d] = model.addVar(0, 1, 0, GRB.BINARY, "y_%d_%d_%d" % (p, t, d))
+                    else:
+                        y[p, t, d] = model.addVar(0, 0, 0, GRB.BINARY, "y_%d_%d_%d" % (p, t, d))  # Set the decision variable to 0 if technician cannot install all machines
+                else:
+                    y[p, t, d] = model.addVar(0, 0, 0, GRB.BINARY, "y_%d_%d_%d" % (p, t, d))  # Set the decision variable to 0 if the distance exceeds the limit
+
+
+    
+    # decision var person p has schedule s
+    z = {}
+    for p in range(1, instance.numTechnicians+1):
+        for s in range(1, len(schedule)):
+            z[p,s] = model.addVar(0, 1, 0, GRB.BINARY, "z_%d_%d" %(p,s))
+
+     # decision variable for total cost of person p
+    c = {}
+    for p in range(1, instance.numTechnicians+1):
+       c[p] = model.addVar(0, GRB.INFINITY, 1, GRB.CONTINUOUS, "c_%d"%p)
+    
+    # constraint that person costs the amount calculated
+    for p in range(1, instance.numTechnicians+1):
+         model.addConstr(quicksum(h[p,t] * y[p,t,d] for t in range(1, len(tours)) \
+                    for d in range(1, instance.days+1)) == c[p])
+    
+    # constraint each person can only work one schedule
+    for p in range(1, instance.numTechnicians+1):
+        model.addConstr(quicksum(z[p,s] for s in range(1, len(schedule))) == 1) 
+    
+    # constraint each person can only work days in their schedule
+    for d in range(1, instance.days+1):
+        for p in range(1, instance.numTechnicians+1):
+            model.addConstr(quicksum(y[p,t,d] for t in range(1, len(tours))) <= \
+                    quicksum(e[s,d]*z[p,s] for s in range(1, len(schedule))))
+    
+    # constraint every machine gets installed
+    for m in range(1, instance.numRequests+1):
+        model.addConstr(quicksum(b[t,m]*y[p,t,d] for t in range(1, len(tours)) for p in range(1, instance.numTechnicians+1) \
+                    for d in range(1, instance.days+1)) == 1) # exactly equal means can't go to request twice
+
+    # constraint machines can't be installed until after the request is released
+    for m in range(1, instance.numRequests+1):
+        for d in range(1, instance.days+1):
+            model.addConstr(quicksum(b[t,m]*y[p,t,d] for t in range(1, len(tours)) for p in range(1, instance.numTechnicians+1)) <=l[m,d])
+
+    #constraint for max requests for a technician
+    for p in range(1, instance.numTechnicians + 1):
+        for t in range(1, len(tours)):
+            for d in range(1, instance.days + 1):
+                model.addConstr(quicksum(b[t, m] * y[p, t, d] for m in tours[t]) <= instance.technicians[p][3])
+
+
+    model.setParam('OutputFlag', False)
+    model.optimize()
+
+     # create list to store y variable values 
+    solutions = [["technician", "tour", "day"]] 
+    machine_days = [] # stores pair with [machine request, day]
+    
+    if model.SolCount == 0:
+        print("***NO SOLUTION FOUND***")
+    else:
+        #parse decision vars and add to list
+        for v in model.getVars():
+            if v.X > 0 and 'y' in v.varName:
+                y,p,t,d = v.varName.split('_')
+                solutions.append([p, tours[int(t)], d])
+                for r in tours[int(t)]:
+                    machine_days.append([int(r), int(d)])
+            
+    return solutions, machine_days
---
+++ Trucks2.py
@@ -0,0 +1,136 @@
+# Trucks2.py is the WORKING IP for solving the truck problem given technician solutions
+
+import numpy as np
+from gurobipy import *
+
+depot = 1
+
+def distance(instance, location1, location2):
+    x1 = instance.locations[location1][1]
+    y1 = instance.locations[location1][2]
+    x2 = instance.locations[location2][1]
+    y2 = instance.locations[location2][2]
+    return np.sqrt((x1-x2)**2 + (y1-y2)**2)
+
+def generate_routes(instance):
+    routes = []
+    locations = list(range(1, instance.numRequests + 1))  # Assuming locations are numbered
+    # Generate routes of length 2
+    for route in itertools.combinations(locations, 2):
+        dist = calculate_route_distance(instance, route)
+        cost = dist * instance.truckDistanceCost
+        routes.append([route, dist, cost])  # Append the route as a list to the routes list
+    return routes
+
+def calculate_route_distance(instance, route):
+    location_1 = instance.requests[route[0]][1]
+    location_2 = instance.requests[route[1]][1]
+    dist = distance(instance, depot, location_1) +  distance(instance, location_1, location_2) + distance(instance, location_2, depot)
+    # Calculate the total distance for a given route
+    return dist
+
+def IP_Trucks(instance, machines):
+
+    # create routes
+    #routes = [["machine", "distance", "cost"]]
+    routes = [[]]
+    for m in range(1, instance.numRequests+1):
+        location_m = instance.requests[m][1]
+        depot = 1
+        dist = distance(instance, location_m, depot) * 2 # going there and back
+        cost = dist * instance.truckDistanceCost
+        routes.append([m, dist, cost])
+    routes = routes + generate_routes(instance) #routs of length 2
+
+    model = Model()
+
+    # decision var is route r performed on day d
+    x = {}
+    for r in range(1,len(routes)):
+        for d in range(1, instance.days+1):
+            x[r,d] = model.addVar(0,1, routes[r][2],  GRB.BINARY, "x_%d_%d" % (r,d)) 
+    
+    # decision var max number trucks used any day
+    n_trucks = model.addVar(0, GRB.INFINITY, instance.truckCost, GRB.INTEGER, "n_trucks")
+
+    # decision var trucks used on day d
+    f = {}
+    for d in range(1, instance.days+1):
+        f[d] = model.addVar(0, GRB.INFINITY, instance.truckDayCost, GRB.CONTINUOUS, "f_%d"%d)
+
+    # decision var number idle days for machine request m
+    w = {}
+    for m in range(1, instance.numRequests+1):
+        #TODO fix this to actually minizie based on idle days
+        w[m] = model.addVar(0, 30, 0, GRB.INTEGER, "w_%d"%m) 
+    
+    # decision var whether route r contains request m
+    a = {}
+    for r in range(1, len(routes)):
+        for m in range(1, instance.numRequests+1):
+            if routes[r][0] == m:
+                a[r,m] = model.addVar(1, 1, 0,  GRB.BINARY, "a_%d_%d" % (r,m)) 
+            else:
+                a[r,m] = model.addVar(0, 0, 0,  GRB.BINARY, "a_%d_%d" % (r,m)) 
+
+   # Decision variable representing the number of trucks purchased
+    num_trucks_purchased = model.addVar(0, GRB.INFINITY, name="num_trucks_purchased")
+   # Add constraint for each day to ensure num_trucks_purchased is greater than or equal to f[d]
+    for d in range(1, instance.days + 1):
+        model.addConstr(num_trucks_purchased >= f[d], "num_trucks_constraint_%d" % d)
+
+    # Add cost associated with purchasing trucks to the objective function
+    truck_purchase_cost = instance.truckCost * num_trucks_purchased
+   
+
+    # constraint every request m is in one route r
+    for m in range(1, instance.numRequests+1):
+        model.addConstr(quicksum(a[r,m]*x[r,d] for r in range(1,len(routes)) for d in range(1, instance.days+1)) == 1)
+
+    # constraint for fleet size 
+    for d in range(1, instance.days+1):
+        model.addConstr(quicksum(x[r,d] for r in range(1,len(routes))) <= n_trucks)
+
+    # constraint for number of trucks on day d
+    for d in range(1, instance.days +1):
+        model.addConstr(quicksum(x[r,d] for r in range(1,len(routes))) == f[d])
+    
+    # constarint for idle days 
+    for m in range(1, instance.numRequests+1):
+        model.addConstr(machines[m-1][1] - quicksum(d*a[r,m]*x[r,d] for d in range(1, instance.days+1) for r in range(1,len(routes))) == w[m])
+    
+    # constraint for earliest delivery and latest delivery (depending on technician schedule)
+    for m in range(1, instance.numRequests+1):
+        model.addConstr(quicksum(a[r,m]*x[r,d] for d in range(1, instance.requests[m][2]) for r in range(1,len(routes)))==0)
+        model.addConstr(quicksum(a[r,m]*x[r,d] for d in range(machines[m-1][1], instance.days+1) for r in range(1,len(routes)))==0)
+        model.addConstr(quicksum(a[r,m]*x[r,d] for d in range(instance.requests[m][3], instance.days+1) for r in range(1,len(routes)))==0)
+    
+    # assume capacity constarint met because routes only contain one request thus never exceed capacity
+
+    model.setObjective(quicksum(f.values()) + truck_purchase_cost, GRB.MINIMIZE)
+    model.setParam('OutputFlag', False)
+    model.optimize()
+
+
+    # create lists to store solutions
+    route_days = [["day", "request"]]
+    num_truck_days = [["num trucks", "day"]]
+    if model.SolCount == 0:
+        print("***NO SOLUTION FOUND***")
+    else:
+        #parse decision vars and add to list
+        for v in model.getVars():
+            if v.X > 0 and 'x' in v.varName:
+                x,r,d = v.varName.split('_')
+                request = routes[int(r)][0]
+                dist = routes[int(r)][1]
+                cost = routes[int(r)][2]
+                route_days.append([int(d), request])
+            if 'f' in v.varName:
+                f,d = v.varName.split('_')
+                num_truck_days.append([int(d), int(v.X)])
+    
+
+    return route_days, num_truck_days
+
+
